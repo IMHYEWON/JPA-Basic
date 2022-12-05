fe7a1d0933a3b0be51b9acf15859865aea2fb9df
IMHYEWON <hyewon1118@gmail.com>
2022-12-05 21:46:22 +0900

    Feat: jpa 트랜잭션 기본 예외처리

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,14 +15,18 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("HelloA");
+        try {
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
 
-        em.persist(member);
-        tx.commit();
-
-        em.close();
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
     }
 }
