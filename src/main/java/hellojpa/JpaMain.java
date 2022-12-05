8a82501b5caea72ac6599a6622d90ade8d0b9a24
IMHYEWON <hyewon1118@gmail.com>
2022-12-05 22:28:03 +0900

    Feat: jpa 기본 Pagination (offset, limit 설정)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,8 +20,10 @@ public class JpaMain {
         try {
             // 기본 JPQL 작성법, 일반적인 쿼리와 다르게 JPA 관점에서 Member 객체를 조회
             List<Member> findMembers = em.createQuery("select m from Member as m", Member.class)
+                // 조회된 결과의 1~10 / offset, limit 설정
+                .setFirstResult(1)
+                .setMaxResults(10)
                 .getResultList();
-
             for (Member member : findMembers) {
                 System.out.println("member.name = " + member.getName());
             }
