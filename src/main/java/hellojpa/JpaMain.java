dcd9040cdf1fdf01f38de2b0943fedde2edb21d4
IMHYEWON <hyewon1118@gmail.com>
2022-12-05 21:48:43 +0900

    Feat: jpa 트랜잭션 기본 조회

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,11 +16,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloB");
-
-            em.persist(member);
+            // 기본 조회
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.id" + findMember.getId());
+            System.out.println("findMember.name" + findMember.getName());
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
